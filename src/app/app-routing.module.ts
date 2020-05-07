226fe5cfb05c508119df7fdbf309469a99aaa629
Quemero94 <matiaserco@gmail.com>
2020-05-07 19:07:34 -0300

    1) y 2) OK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BienvenidaComponent } from './componentes/bienvenida/bienvenida.component';
+import { BusquedaComponent } from './componentes/busqueda/busqueda.component';
+import { PeliculaAltaComponent } from './componentes/pelicula-alta/pelicula-alta.component';
+import { ActorAltaComponent } from './componentes/actor-alta/actor-alta.component';
+import { ActorListadoComponent } from './componentes/actor-listado/actor-listado.component';
+import { PeliculasListadoComponent } from './componentes/peliculas-listado/peliculas-listado.component';
+import { PaisesListadoComponent } from './componentes/paises-listado/paises-listado.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: BienvenidaComponent },
+  { path: 'Busqueda', component: BusquedaComponent },
+
+  {
+    path: 'Peliculas',
+    children: [
+      { path: 'lista', component: PeliculasListadoComponent },
+      { path: 'alta', component: PeliculaAltaComponent }
+    ]
+  },
+  {
+    path: 'Actor',
+    children: [
+      { path: 'lista', component: ActorListadoComponent },
+      { path: 'alta', component: ActorAltaComponent }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenidaComponent } from './componentes/bienvenida/bienvenida.component';
+import { BusquedaComponent } from './componentes/busqueda/busqueda.component';
+import { PeliculaAltaComponent } from './componentes/pelicula-alta/pelicula-alta.component';
+import { ActorAltaComponent } from './componentes/actor-alta/actor-alta.component';
+import { ActorListadoComponent } from './componentes/actor-listado/actor-listado.component';
+import { PeliculasListadoComponent } from './componentes/peliculas-listado/peliculas-listado.component';
+import { PaisesListadoComponent } from './componentes/paises-listado/paises-listado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidaComponent,
+    BusquedaComponent,
+    PeliculaAltaComponent,
+    ActorAltaComponent,
+    ActorListadoComponent,
+    PeliculasListadoComponent,
+    PaisesListadoComponent
   ],
   imports: [
     BrowserModule,
